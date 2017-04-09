5b178bbb2e212e90c6fca23020b0dfbab5d5f53f
Gedas <gediminas.zinkevicius@gmail.com>
2017-04-10 01:13:30 +0300

    basic item storing

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'type',
+        'description',
+        'expirationDate',
+        'quantity',
+        'startingBid',
+        'picture'
+    ];
+
+    public static function createItem(Item $user_id, $title, $type, $description, $expirationDate, $quantity, $startingBid, $picturePath){
+        $item = Item::fill([
+            'user_id' => $user_id,
+            'title' => $title,
+            'type' => $type,
+            'description' => $description,
+            'expirationDate' => $expirationDate,
+            'quantity' => $quantity,
+            'startingBid' => $startingBid,
+            'picture' => $picturePath
+        ]);
+        $item->save();
+    }
 }
